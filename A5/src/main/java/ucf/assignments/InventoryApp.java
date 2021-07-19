bc72be8b7a1e4e0b44be165f7951934993373c79
Kevin Desktop <kevinkant@knights.ucf.edu>
2021-07-19 19:22:17 -0400

    Initial Commit with initial Menu Design and UML Design. ZERO functionality besides launching menu

---
+++ A5/src/main/java/ucf/assignments/InventoryApp.java
@@ -0,0 +1,34 @@
+package ucf.assignments;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class InventoryApp extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("Menu.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Stage stage = new Stage();
+        Scene scene = new Scene(root);
+        stage.setTitle("Single Save");
+        stage.setScene(scene);
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.show();
+    }
+}
---
+++ A5/src/main/java/ucf/assignments/MenuController.java
@@ -0,0 +1,23 @@
+package ucf.assignments;
+
+import javafx.event.ActionEvent;
+
+public class MenuController {
+    public void NewMenuButtonClicked(ActionEvent actionEvent) {
+    }
+
+    public void SaveInventoryButtonClicked(ActionEvent actionEvent) {
+    }
+
+    public void LoadInventoryButtonClicked(ActionEvent actionEvent) {
+    }
+
+    public void AddItemButtonClicked(ActionEvent actionEvent) {
+    }
+
+    public void DeleteItemButtonClicked(ActionEvent actionEvent) {
+    }
+
+    public void SearchButtonClicked(ActionEvent actionEvent) {
+    }
+}
